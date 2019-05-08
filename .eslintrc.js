928e27e3a8452f6bb7f648d99fbe0a3b2155a5b3
Marcus Klein <m.klein@livello.com>
2019-05-08 15:02:01 +0200

    fix eslint module resolver

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'plugin:import/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    'import/no-extraneous-dependencies': 'off',
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: 'config/webpack/webpack.config.dev.js',
+      },
+    },
+  },
+};
--- src/core/reducers.js
+++ src/core/reducers.js
@@ -1,9 +1,9 @@
 import { combineReducers } from 'redux';
-import core from './reducers/coreReducer';
 
 // Shared reducers
 import user from 'modules/authentication/reducers/userReducer';
-import organizations from 'modules/organizations/reducers/organizationsReducer.js';
+import organizations from 'modules/organizations/reducers/organizationsReducer';
+import core from './reducers/coreReducer';
 
 // Module reducers
 export default combineReducers({ core, user, organizations });
