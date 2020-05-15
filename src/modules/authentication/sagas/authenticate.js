a3e9b52a060f203126ae02dd1b8f9d9f3f913c2d
Volodymyr Romanets <volodymyr.romanets@brainbeanapps.com>
2020-05-15 18:44:36 +0300

    BBA/Cache-fixing Fixed

--- src/modules/authentication/sagas/authenticate.js
+++ src/modules/authentication/sagas/authenticate.js
@@ -38,6 +38,7 @@ function parseHash() {
   return new Promise((resolve, reject) => {
     auth.parseHash((err, authResult) => {
       if (authResult && authResult.accessToken && authResult.idToken) {
+        console.log('fgdgdfg');
         resolve(authResult);
       } else if (err) {
         reject(err);
@@ -112,6 +113,7 @@ export function* renewSession() {
         ...userDetails,
       });
       yield LivelloLS.setItem(TOKEN_STORAGE_KEY, result.accessToken);
+      window.history.replaceState('', document.title, ' ');
     } catch (e) {
       console.log(e);
       // TODO: Some error notification
