71fccdfee32ab85a7103a03b62d14bb64b733918
f.susana <f.susana@livello.com>
2020-07-29 13:05:12 +0200

    Trying with another request method.

--- src/modules/transactions/sagas/exportCsv.js
+++ src/modules/transactions/sagas/exportCsv.js
@@ -8,21 +8,43 @@ import {
 } from '../actions';
 
 export function handlerGetProduct() {
+
   const token = ls.getItem(TOKEN_STORAGE_KEY);
-  console.log('token:', token)
-  return fetch(`/api/v1/transactions/csv/export/1578328700000/1578388700000`, {
-//   fetch(`/api/v1/transactions/csv/export/1578328700000/1578388700000`, {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
-    },
-  })
+
+  fetch(`/api/v1/transactions/csv/export/1578328700000/1578388700000`, {
+  method: 'GET',
+  // headers: new Headers({
+  //     "Authorization": "Bearer " + token
+  // })
+  headers: {
+    Authorization: `Bearer ${token}`,
+    'Content-Type': 'application/json',
+  },
+})
+.then(response => response.blob())
+.then(blob => {
+  var url = window.URL.createObjectURL(blob);
+  var a = document.createElement('a');
+  a.href = url;
+  a.download = "filename.xlsx";
+  document.body.appendChild(a); // we need to append the elem
+})
+}
+//   const token = ls.getItem(TOKEN_STORAGE_KEY);
+//   console.log('token:', token)
+//   return fetch(`/api/v1/transactions/csv/export/1578328700000/1578388700000`, {
+// //   fetch(`/api/v1/transactions/csv/export/1578328700000/1578388700000`, {
+//     method: 'GET',
+//     headers: {
+//       Authorization: `Bearer ${token}`,
+//       // 'Content-Type': 'application/json',
+//     },
+//   })
 //   .then((response) => {
 //       console.log('response-0: ', response)
 //     return response
 //   });
-}
+// }
 
 
 function* handler({ payload }) {
@@ -31,15 +53,17 @@ function* handler({ payload }) {
     const response = yield call(handlerGetProduct);
     console.log('payload: ', payload)
     console.log('response-1: ', response)
-    if (response.status !== 200) {
-        throw Error('Error in saga');
-    }
+    // if (response.status !== 200) {
+    //     throw Error('Error in saga');
+    // }
+    
     // const data = yield call([response, response.json]);
     // const data = yield call(response, null);
     // console.log('data: ', data)
-    yield put(actionSuccess({
-        smth: response
-    }));
+    
+    // yield put(actionSuccess({
+    //     smth: response
+    // }));
     // console.log('response.body:', response.body)
     // yield put(actionSuccess(response));
   } catch (e) {
