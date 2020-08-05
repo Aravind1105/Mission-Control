c893af511c0ee3785c2ef88ab05bce0663d11732
Ramu Ramasamy <r.ramasamy@livello.com>
2020-08-05 10:35:16 +0200

    style(Login Button): LIV-1632 Login button icon removed

--- src/modules/authentication/components/LoginButton.js
+++ src/modules/authentication/components/LoginButton.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Button, Icon } from 'semantic-ui-react';
+import { Button } from 'semantic-ui-react';
 
 import { authenticateUserSaga } from 'modules/authentication/actions';
 
 const LoginButton = ({ authUser, ...props }) => (
-  <Button primary onClick={authUser} icon labelPosition="left" {...props}>
-    <Icon name="google" />
+  <Button positive onClick={authUser} {...props}>
     Log in
   </Button>
 );
