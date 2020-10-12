4fd1902e4a1c7ce44407f0ae432039da11267b0b
Ramu Ramasamy <r.ramasamy@livello.com>
2020-10-12 10:34:34 +0200

    fix(price history date format): LIV-1951

--- src/modules/products/components/ProductPriceHistory.js
+++ src/modules/products/components/ProductPriceHistory.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Segment, Divider } from 'semantic-ui-react';
+import format from 'date-fns/format';
 
 const ProductPriceHistory = ({ priceHistory, kiosks }) => {
   const filtered = priceHistory ? priceHistory.filter(el => !el.default) : [];
@@ -12,7 +13,7 @@ const ProductPriceHistory = ({ priceHistory, kiosks }) => {
       <p>{`Default: ${defaultPrice ? defaultPrice.price : 0}€`}</p>
       <div>
         {filtered.map(({ validForKiosk, updated, price, _id }) => {
-          const date = new Date(updated).toLocaleDateString();
+          const date = format(new Date(updated), 'dd-MM-yyyy, HH:mm:ss');
           const kioskName = kiosks[validForKiosk] || 'noname';
           return <p key={_id}>{`${kioskName}: ${price}€ - ${date}`}</p>;
         })}
