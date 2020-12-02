7c5f12f21b97b0fe87a6f93957f7eaf1c12523f0
Ramu Ramasamy <r.ramasamy@livello.com>
2020-12-02 13:28:04 +0100

    style(toolbar):LIV-2420 redesigned user dropdown menu

--- src/modules/shared/components/UserProfileBar/index.js
+++ src/modules/shared/components/UserProfileBar/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Dropdown, Image } from 'semantic-ui-react';
 
+import history from 'lib/history';
 import { getUserState } from 'modules/authentication/selectors';
 import { logoutUserSaga } from 'modules/authentication/actions';
 import UserElement from './UserElement';
@@ -21,13 +22,19 @@ const UserProfileBar = ({
   return (
     <Dropdown trigger={trigger} pointing="top right" icon={null}>
       <Dropdown.Menu>
-        <Dropdown.Item
-        // icon="sign-out"
-        // text="Sign out"
-        // onClick={logoutUserSaga}
-        >
+        <Dropdown.Item>
           <UserElement name={name} email={email} avatar={picture} />
         </Dropdown.Item>
+        <Dropdown.Item
+          icon="setting"
+          text="Settings"
+          onClick={() => history.push("/settings/security")}
+        />
+        <Dropdown.Item
+          icon="sign-out"
+          text="Sign out"
+          onClick={logoutUserSaga}
+        />
       </Dropdown.Menu>
     </Dropdown>
   );
