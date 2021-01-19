d46ee5b4d48acf3401c00bf97ecf11e45f669559
Rana <r.faheem@livello.com>
2021-01-19 15:41:10 +0100

    feature(dashboard,kiosk,transactions): update the name of kiosks.

--- src/modules/dashboard/components/Toolbar.js
+++ src/modules/dashboard/components/Toolbar.js
@@ -30,7 +30,7 @@ const Toolbar = ({
       <Grid.Row verticalAlign="middle">
         <Grid.Column width={4}>
           <Dropdown
-            placeholder="All Kiosks"
+            placeholder="Kiosk"
             selection
             options={kiosks}
             className="full-width"
