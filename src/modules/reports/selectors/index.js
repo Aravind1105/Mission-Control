86969030714233a55c1e2fbc7f507dc2e65e26fa
Aravind Madesan <aravind.madesan@gmail.com>
2021-09-29 09:09:47 +0000

    Revert "Merge branch 'test-master' into 'staging-master'"
    This reverts merge request !577

--- src/modules/reports/selectors/index.js
+++ src/modules/reports/selectors/index.js
@@ -2,3 +2,37 @@ import { createSelector } from 'reselect';
 import get from 'lodash/get';
 
 export const getReports = state => state.reports;
+
+export const getSalesListReports = createSelector(getReports, ({ data }) => {
+  const { lastMonth = [], income = [], totalRevenue = [] } = data || {};
+  return { lastMonth, income, totalRevenue };
+});
+
+export const getTargetStatistics = createSelector(getReports, ({ data }) => {
+  const { income = 0, expenses = 0, spending = 0, total = 0 } = get(
+    data,
+    'targetStatistics',
+    {},
+  );
+  return { income, expenses, spending, total };
+});
+
+export const getTargetSales = createSelector(getReports, ({ data }) => {
+  const { targetSales } = data || {};
+  return targetSales;
+});
+
+export const getPortfolioStatistic = createSelector(getReports, ({ data }) => {
+  const { portfolioStatistic = null } = data || {};
+  return portfolioStatistic;
+});
+
+export const getTopSellers = createSelector(getReports, ({ data }) => {
+  const { sellers = null } = data || {};
+  return sellers;
+});
+
+export const getBestSellingProducts = createSelector(getReports, ({ data }) => {
+  const { bestSellingProducts = null } = data || {};
+  return bestSellingProducts;
+});
