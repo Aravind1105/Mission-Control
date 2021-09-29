dc32bda532e5fa2d168ca9f37de73650c8c5ad2d
Steliyan Dinkov <s.dinkov@livello.com>
2021-09-29 07:19:23 +0000

    Revert "Merge branch 'LIV-3871' into 'test-master'"

--- src/modules/reports/index.js
+++ src/modules/reports/index.js
@@ -1,23 +1,9 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Grid, Header, Segment, Button, Icon } from 'semantic-ui-react';
-
-import OneLineChart from './components/OneLineChart';
-import TotalStatisticContainer from './components/TotalStatisticContainer';
-import ComplexChart from './components/ComplexChart';
-import PortfolioContainer from './components/PortfolioContainer';
-import TopSellers from './components/TopSellers';
-import BestSellerProducts from './components/BestSellerProducts';
-import PortfolioPerfGraph from './components/PortfolioPerfGraph';
 import { getReports } from './actions';
-import {
-  getSalesListReports,
-  getTargetStatistics,
-  getTargetSales,
-  getPortfolioStatistic,
-  getTopSellers,
-  getBestSellingProducts,
-} from './selectors';
+import StatsCard from 'modules/shared/components/StatsCard';
+import {} from './selectors';
 import './styles.less';
 
 const options = {
@@ -35,19 +21,8 @@ const options = {
   },
 };
 
-const Reports = ({
-  portfolioStatistic,
-  salesListReports,
-  targetStatistics,
-  targetSales,
-  topSellers,
-  bestSellingProducts,
-  isLoading,
-  getReports,
-}) => {
-  useEffect(() => {
-    if (!isLoading) getReports();
-  }, []);
+const Reports = ({ isLoading }) => {
+  useEffect(() => {}, []);
 
   return (
     <>
@@ -58,36 +33,51 @@ const Reports = ({
         </Header>
       </Segment>
 
-      <PortfolioContainer statistic={portfolioStatistic} />
-
       <Grid>
-        <Grid.Row columns="equal">
-          {Object.keys(salesListReports).map(key => (
-            <Grid.Column key={key}>
-              <OneLineChart
-                data={salesListReports[key]}
-                title={options[key].title}
-                color={options[key].color}
-              />
-            </Grid.Column>
-          ))}
-        </Grid.Row>
-      </Grid>
-
-      <ComplexChart data={targetSales} />
-
-      <TotalStatisticContainer statistic={targetStatistics} />
-
-      <Grid>
-        <Grid.Row columns="equal">
-          <Grid.Column>
-            <TopSellers data={topSellers} />
+        <Grid.Row>
+          <Grid.Column mobile={16} computer={4} tablet={8}>
+            <StatsCard
+              customColor="#219653"
+              text="Total Net Sales"
+              amount={`22 €`}
+            />
           </Grid.Column>
-          <Grid.Column>
-            <BestSellerProducts data={bestSellingProducts} />
+          <Grid.Column mobile={16} computer={4} tablet={8}>
+            <StatsCard
+              customColor="#F2994A"
+              text="Total Products Sold"
+              amount={`23 €`}
+            />
           </Grid.Column>
-          <Grid.Column>
-            <PortfolioPerfGraph data={portfolioStatistic} />
+          <Grid.Column mobile={16} computer={4} tablet={8}>
+            <StatsCard
+              customColor="#56CCF2"
+              text="Peak Hour"
+              amount={`-- €`}
+            />
+            {/* <StatsCard
+              icon="boxes"
+              customColor="#F2994A"
+              text="Total Products sold"
+              amount={widgetsData.totalNumberOfProductsSold
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+            /> */}
+          </Grid.Column>
+          <Grid.Column mobile={16} computer={4} tablet={8}>
+            <StatsCard
+              customColor="#BB6BD9"
+              text="Average Daily Net Sales"
+              amount={`-- €`}
+            />
+            {/* <StatsCard
+              icon="credit card"
+              customColor="#2F80ED"
+              text="Total Transactions"
+              amount={widgetsData.totalNumberOfTransactions
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+            /> */}
           </Grid.Column>
         </Grid.Row>
       </Grid>
@@ -108,12 +98,6 @@ const Reports = ({
 };
 
 const mapStateToProps = state => ({
-  salesListReports: getSalesListReports(state),
-  targetStatistics: getTargetStatistics(state),
-  targetSales: getTargetSales(state),
-  portfolioStatistic: getPortfolioStatistic(state),
-  topSellers: getTopSellers(state),
-  bestSellingProducts: getBestSellingProducts(state),
   isLoading: state.reports.isLoading,
 });
 
