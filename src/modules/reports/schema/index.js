967e3f283fb2918630a0aa9ec97bf58b5b661318
Ramu Ramasamy <r.ramasamy@livello.com>
2021-10-05 09:15:32 +0200

    feat(reports widget):LIV-3294 average daily revenue and peak sales hour widgets

--- src/modules/reports/schema/index.js
+++ src/modules/reports/schema/index.js
@@ -1,9 +1,15 @@
 import gql from 'graphql-tag';
 
 export const GET_REPORTS_WIDGET_DATA = gql`
-  query($period: Period!, $kioskId: [String]) {
+  query($period: Period!, $kioskId: [String], $kioskIds: [String]) {
     getTotalNetIncome(period: $period, kioskId: $kioskId)
     getTotalNumberOfProductsSold(period: $period, kioskId: $kioskId)
+    getAverageDailyNetRevenue(period: $period, kioskIds: $kioskIds)
+    getPeakSalesHour(period: $period, kioskIds: $kioskIds) {
+      start
+      end
+      sum
+    }
   }
 `;
 
